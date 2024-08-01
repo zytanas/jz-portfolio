0a556d36fa5d977f35e96e859929b184c4f14cfc
zytanas <juliazyrene23@gmail.com>
2024-08-01 14:35:38 +0800

    updated home page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,36 +1,31 @@
 import React from 'react'
 import "./Hero.css"
-import Avatar from '../../Images/id.png'
-import HtmlIcon from '../../Images/html.webp'
 import CssIcon from '../../Images/css.webp'
-import JsIcon from '../../Images/js.png'
 import ReactIcon from '../../Images/react.png'
+import Profile from '../../Images/profile.jpg'
 
 const Hero = () => {
   return (
     <section id='home' className='hero-container'>
         <div className='hero-content'>
-            <h2>Creating Inspiring Digital Experiences</h2>
+            <h2>Julia Zyrene Almoite</h2>
             <p>Dedicated Frontend Developer | Breathing life into ideas, one pixel at a time, to craft visually stunning web solutions that captivate and inspire.</p>
         </div>
 
         <div className='hero-img'>
             <div>
-                    <img  src={Avatar} alt=''/>
+                    <img  src={Profile} alt=''/>
             </div>
-
+            
+            <p>Developed using:</p>
+    
             <div>
                 <div className='tech-icon'>
-                    <img src={HtmlIcon} alt=''/>
+                    <img src={ReactIcon} alt=''/>
                 </div>
+            
                 <div className='tech-icon'>
-                    <img src={CssIcon} alt=''/>
-                </div>
-                <div className='tech-icon'>
-                    <img src={JsIcon} alt=''/>
-                </div>
-                <div className='tech-icon'>
-                <img src={ReactIcon} alt=''/>
+                <img src={CssIcon} alt=''/>
                 </div>
             </div>
         </div>
