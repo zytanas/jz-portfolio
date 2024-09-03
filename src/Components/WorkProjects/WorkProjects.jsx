73e6c637bc6abdaeb868950c3d6fe3ef5e3e83d8
zytanas <juliazyrene23@gmail.com>
2024-09-03 16:54:21 +0800

    work projects hotfix

--- src/Components/WorkProjects/WorkProjects.jsx
+++ src/Components/WorkProjects/WorkProjects.jsx
@@ -5,7 +5,6 @@ import RedHorseBeer from "./Rhb.png";
 import Vndci from "./Vndci.png";
 import Bank from "./Bank.png";
 
-
 const projects = [
   {
     id: 1,
@@ -14,8 +13,8 @@ const projects = [
     title: "Online Banking Dashboard ",
     description:
       "A sleek and secure online banking dashboard offering real-time account management, transaction tracking, and personalized financial insights for effortless financial control.",
-    tags:"Figma"
-    },
+    tags: "Figma",
+  },
   {
     id: 2,
     src: Vndci,
@@ -23,32 +22,26 @@ const projects = [
     title: "VNDCI",
     description:
       "Web-based inventory system built, designed to streamline and optimize inventory management with a user-friendly interface and robust performance.",
-  tags:"MERN Stack & Tailwind CSS"
+    tags: "MERN Stack & Tailwind CSS",
   },
   {
     id: 3,
     src: RedHorseBeer,
     alt: "Red Horse Beer",
     title: "Red Horse Beer",
-    description: "A modern recreation of the Red Horse Beer webpage designed in Figma, blending bold visuals with intuitive navigation to enhance brand identity.",
-    tags:"Figma"
+    description:
+      "A modern recreation of the Red Horse Beer webpage designed in Figma, blending bold visuals with intuitive navigation to enhance brand identity.",
+    tags: "Figma",
   },
-
 ];
 
-
 const WorkProjects = () => {
-
-
   return (
     <section id="projects" className="project-container">
       <h5>Projects</h5>
       <div className="project-contents">
         {projects.map((project) => (
-          <div
-            key={project.id}
-            className="project-item"
-          >
+          <div key={project.id} className="project-item">
             <img src={project.src} alt={project.alt} />
             <h6>{project.title}</h6>
             <p>{project.description}</p>
@@ -56,7 +49,6 @@ const WorkProjects = () => {
           </div>
         ))}
       </div>
-
     </section>
   );
 };
